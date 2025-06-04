226eb36c1786a438b00f9a5da3f48e3bad0b2d17
Jacob Williams <jmw992@gmail.com>
2025-06-03 21:11:30 -0400

    feat: add no background SVG & PNG

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -36,9 +36,9 @@ export default function AppLayout({
                   <div className="shrink-0">
                     <img
                       alt="Waaaghlyzer logo"
-                      src="/WA.svg"
+                      src="/WA-NoBackground.svg"
                       // src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                      className="size-8"
+                      className="size-10"
                     />
                   </div>
                 </button>
